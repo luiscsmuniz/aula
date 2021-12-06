6d38a2ef901afb7ce77b6a56abc9ccc1d477ee86
Luis Carlos de Souza <47360932+luiscsmuniz@users.noreply.github.com>
2021-12-06 01:16:48 +0000

    Condições

---
+++ aula_02/bhaskara.js
@@ -0,0 +1,32 @@
+/**
+ * delta = b^2 - 4 *a * c
+ * x1 = (-b - sqrt(delta)) / (2 * a)
+ * x2 = (-b + sqrt(delta)) / (2 * a)
+ */
+
+const a = 2
+const b = 12
+const c = -14
+
+const delta = (b * b) - (4 * a * c)
+
+console.log(delta)
+
+const bhaskara = {
+  x1: (-b - Math.sqrt(delta)) / (2 * a),
+  x2: (-b + Math.sqrt(delta)) / (2 * a),
+}
+
+console.log(bhaskara)
+
+if (bhaskara.x1 < 0) {
+  console.log('X1 é negativo')
+} else {
+  console.log('X1 é positivo')
+}
+
+if (bhaskara.x2 < 0) {
+  console.log('X2 é negativo')
+} else {
+  console.log('X2 é positivo')
+}
---
+++ aula_02/exercicio/celulol.js
@@ -0,0 +1,16 @@
+/**
+ * LISTAR PELO MENOS 15 CAMPEOES DO CELULOL
+ * 1 -  Organizar por nome, rota, range (meele ou ranged), classe (lutador, mago, tanks, assassinos), dano (ap, ad), genero (masculino, feminino)
+ * 2- Listar todos os tanks do top
+ * 3- listar todos os mids magos
+ * 4- listar todos os junglers Ad
+ * 5- separar por genero
+ * 6- separar por rota
+ * 7- separar ad e ap
+ * 8- retornar somente nome, rota e dano
+ */
+
+const campeoes = [
+  {nome: 'camille'},
+
+]
